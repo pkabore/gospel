6688216d485a03fcf58c0483d6744ce62840d24b
pkabore <pkabore@tutanota.com>
2022-10-10 21:56:30 +0000

    Tailwindcss setup and typographic scales
    - Init npm
    - Add tailwindcss, prettier and prettier-plugin-tailwindcss npm dev dependencies
    - Add typographic scale styles
    - Add Inter and Urbanist fonts
    - Create the three html files
    - Add basic layout and styles to index.html

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'public/**/*.html'
+  ],
+  theme: {
+    extend: {},
+    fontFamily:{
+      sans: ["Inter", "system-ui"],
+      display: ["Urbanist", 'Inter', 'system-ui']
+    }
+  },
+  plugins: [],
+}
