969f9e4a05cceb5a9d2472a7243f1ab4a8150cfc
pkabore <pkabore@tutanota.com>
2022-10-11 18:17:06 +0000

    change css build output dir

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["public/**/*.html"],
+  content: ["*.html"],
   theme: {
     extend: {
       colors: {
